de8a4238ddccd7bdc925ac3c77cddca58ac6662f
JuanJose-Richard <mcvr6661@gmail.com>
2021-11-22 01:31:49 -0400

    Replicando tarea de clase

---
+++ miAppDjango/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+#Rutas para la aplicacion no para el proyecto
+#las comillas vacias son como la /
+urlpatterns = [
+    path('',views.index, name='index'), 
+]
